ba612ee86944577e5a2c18568518fc68bc504706
markuskrash <markgrechnev@gmail.com>
2024-04-28 23:09:32 +0300

    Доработал мидлваоры

--- routes/games.js
+++ routes/games.js
@@ -1,12 +1,18 @@
 const gamesRouter = require("express").Router();
-const { readData, writeData } = require("../utils/data");
-const {sendAllGames, addGameController, deleteGame} = require("../controllers/games")
-const {getAllGames} = require("../middlewares/games");
-
+const {readData, writeData} = require("../utils/data");
+const {
+    getAllGames,
+    deleteGame,
+    checkIsTitleInArray,
+    findGameById,
+    updateGamesArray,
+    updateGamesFile
+} = require("../middlewares/games");
+const {sendAllGames, sendUpdatedGames} = require("../controllers/games")
 
 gamesRouter.get("/games", getAllGames, sendAllGames);
-gamesRouter.post("/games", getAllGames, addGameController);
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+gamesRouter.post("/games", getAllGames,checkIsTitleInArray, updateGamesArray, updateGamesFile, sendUpdatedGames);
+gamesRouter.delete("/games/:id", getAllGames, findGameById, deleteGame, updateGamesFile, sendUpdatedGames);
 
 
 module.exports = gamesRouter;
